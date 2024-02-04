00166e22c17a616abad47d344f5e21a5295a56ef
GabriRoss <rossatogabriele096@gmail.com>
2024-02-04 19:53:16 +0100

    Finita funzione che crea e inizializza l'arrey delle tessere standard e prima bozza della funzione per stampare le tessere. Aggiornata lista TODO

--- main.c
+++ main.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+
+//TIPI VARIABILI STRUCT (TIENI QUI PERCHE ALTRIMENTI FUNZIONE N3 NON VA)
+typedef struct {
+    int num1;
+    int num2;
+} tessera;
 
 //----------------------FUNZIONI FIRME---------------------------//
 void style();
 int playMode();
-void error(char *message);
-void getTessere();
+void error(const char *message);
+tessera* stdTessere(); // FUNZIONE N3
+void printTessere(tessera* a, int size);
 //---------------------------------------------------------------//
-
-//TIPI VARIABILI STRUCT
-typedef struct {
-    num1;
-    num2;
-} tessera;
 
 int main()
 {
-    
+    // TENIAMO I PUNTATORI DELLA MEMEORIA ALLOCATA NEL MAIN
+    tessera* std = stdTessere();
+
     // INTRODUZIONE
     printf("BENVENUTO IN DOMINO\n");
+
     style();
     int modalita = playMode();
     style();
@@ -37,6 +42,8 @@ int main()
         error("ERRORE\n");
     }
 
+    //FREE ALL MALLOC CREATED!!!!!!!!!!
+    free(std);
     return 0;
 };
 
@@ -82,20 +89,41 @@ void error(const char *message)
     printf("ERRORE: %s", message);
 };
 
-void getTessere()
+tessera* stdTessere()
 {   
-    /*tessera std[21];
-    tessara card;
-    for (int i = 0; i < 21; ++i) {
-        for (int a = 1; a <= 6; ++a){
-            std[0] = 
+    const int lunghezza = 21;
+    const int maxValue = 6;
+    // STD NOME UNIVOCO PER NON FARE CONFUSIONE
+    // EQUIVALENTE CODICE ---> tessera std[21]; 
+    tessera* std = (tessera*)malloc(sizeof(tessera) * lunghezza); 
+    if (std == NULL) {
+    // GESTIONE ERRORE ALLOCAZAIONE
+    error("Errore di allocazione di memoria");
+    // GESTIONE ERRORE ALLOCAZIONE
+    exit(EXIT_FAILURE);
+    }
+    //INIZIALIZZAZIONE TUTTE 21 TESSERE BASE/STANDARD (str)
+    int i = 0;
+    // DISEGNO GABRI WHATSAPP 
+    for(int n1 = 1; n1 <= maxValue; ++n1){ 
+        // N1 PARTENZA PRIMO NUMERO ARRAY
+        for(int n2 = n1; n2 <= maxValue; ++n2) {
+            // N2 PARTE DAL VALORE DI N1
+            std[i].num1 = n1;
+            std[i++].num2 = n2;
         }
-
-    }*/
-
+    }
+    return std;
 } 
+
+void printTessere (tessera* a, int size) {
+    for (int i = 0; i < size; ++i){
+        printf("[%d|%d] ", a[i].num1, a[i].num2);
+    }
+    puts("");
+}
 
 // MODALITA' CLASSICA
 void mod1(){
-    tessera* std[] = getTessere();
+    //tessera* std[] = getTessere();
 }
