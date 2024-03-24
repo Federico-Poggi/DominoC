0aa77cbe0b82dbfcdf4246e113782355367a9bbf
GabriRoss <rossatogabriele096@gmail.com>
2024-03-24 13:20:51 +0100

    commit test1

--- src/modAI.c
+++ src/modAI.c
@@ -7,18 +7,59 @@
 #include "domino.h"
 #include "check.h"
 
-void modAI(tessera* alltessere, int tableSize) {
+typedef struct
+{
+    tessera card;
+    int freq;
+} cardObj; // ogetto carta che contiene tessera e sua frequenza
 
-    tessera* table = creaTable(tableSize);
+int findDim (tessera *AiCards, int tableSize) {
+    int count = 1; // prima tessera non ha doppioni
+    for (int i = 1; i < tableSize; i++) {
+        count++;
+        for (int j = i-1; j >= 0; j--)  {
+            if ((AiCards[i].num1 == AiCards[j].num1) && (AiCards[i].num2 == AiCards[j].num2)) {
+                count--;
+            }
+        }
+    }
+    return count;
+}
 
-    tessera* tableCard = giveTessereToPlayer(alltessere, tableSize);
-    printTessere(tableCard, tableSize);
+cardObj *createArray (tessera *AiCards, int tableSize) {
+    int sizeObj = findDim (AiCards, tableSize);
+    cardObj *output = (cardObj *)malloc(sizeof(cardObj) * sizeObj);
 
-    int most_freq = findMostFrequent(tableCard, tableSize);
-    // printf("%d", most_freq);
+    /*
+
+    roba simile a prima du find dim ma con l'array obj
+
+    output[0].card 
+    for (int i = 1; i < tableSize; i++) {
+        for (int j = 0; j < tableSize; j++)  {
+            
+        }
+    } 
+    */
 
 
+
+
+    return output;
+}
+
+void modAI(tessera *alltessere, int tableSize) {
+
+    tessera *table = creaTable(tableSize);
+
+    tessera *AiCards = giveTessereToPlayer(alltessere, tableSize);
+    printTessere(AiCards, tableSize);
+
+    //int sizeObj = findDim (AiCards, tableSize); 
+    cardObj *arrayObj = createArray (AiCards, tableSize);
 
     free(table);
-    free(tableCard);
+    free(AiCards);
+    free(arrayObj);
 }
+
