ad28c89d21d67193fb980d08344de6356d443950
GabriRoss <rossatogabriele096@gmail.com>
2024-03-24 19:23:41 +0100

    sorting funzionante

--- src/modAI.c
+++ src/modAI.c
@@ -51,29 +51,31 @@ cardObj *createArray (tessera *AiCards, int tableSize) {
 */
 
 void swap (tessera *AiCards1, tessera *AiCards2) {
-    int temp1 = AiCards1->num1;
-    AiCards1->num1 = AiCards2->num1;
-    AiCards2->num1 = temp1;
-
-    int temp2 = AiCards1->num2;
-    AiCards1->num2 = AiCards2->num2;
-    AiCards2->num2 = temp2;
+    tessera temp1 = *AiCards1;
+    *AiCards1 = *AiCards2;
+    *AiCards2 = temp1;
 }
 
-void sortCards(tessera *AiCards, int tableSize) {
+void sortCards(tessera *AiCards, int Size) {
+    
     // Sort num1
-    for (int i = 0; i < tableSize - 1; i++) {
-        for (int j = 0; j < tableSize - 1; j++) {
+    for (int i = 0; i < Size - 1; i++) {
+        for (int j = 0; j < Size - 1; j++) {
             if (AiCards[j].num1 > AiCards[j+1].num1) {
                 swap(&AiCards[j], &AiCards[j+1]);
             }
         }
     }
 
-   
+    // Sort num2 
+    for (int i = 0; i < Size - 1; i++) {
+        for (int j = 0; j < Size - 1; j++) {
+            if ((AiCards[j].num1 == AiCards[j+1].num1) && (AiCards[j].num2 > AiCards[j+1].num2)) {
+                swap(&AiCards[j], &AiCards[j+1]);
+            }
+        }
+    }
 }
-
-
 
 void modAI(tessera *alltessere, int tableSize) {
 
