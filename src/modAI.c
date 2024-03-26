0fc426bb6be86f4763c8c8eb771d280f6ea5f820
GabriRoss <rossatogabriele096@gmail.com>
2024-03-26 19:47:31 +0100

    monezzaCommit

--- src/modAI.c
+++ src/modAI.c
@@ -6,7 +6,6 @@
 #include "domino.h"
 #include "check.h"
 
-
 int findDim(tessera* AiCards, int tableSize) {
     int count = 0; // Inizializziamo il conteggio a 0
     int i = 0;
@@ -20,6 +19,14 @@ int findDim(tessera* AiCards, int tableSize) {
         i = j;
     }
     return count;
+}
+
+tessera rotateAI(tessera playerCards1)
+{
+    int temp = playerCards1.num1; // variabile temporanea salvata per lo swap
+    playerCards1.num1 = playerCards1.num2;
+    playerCards1.num2 = temp;
+    return playerCards1;
 }
 
 cardObj* createArray(tessera* AiCards, int tableSize, int size) {
@@ -76,6 +83,42 @@ void sortCards(tessera* AiCards, int Size) {
     }
 }
 
+int obtainCard(cardObj* arrayObj, int sizeObj) {
+    int max = arrayObj[0].freq;
+    int totalPoint = 0;
+    int tempPoint = 0; 
+    int index1 = 0;
+    for (int i = 1; i < sizeObj; i++) {
+        tempPoint = arrayObj[i].card.num1 + arrayObj[i].card.num2;
+        if (arrayObj[i].freq >= max && totalPoint < tempPoint) {
+           max = arrayObj[i].freq;
+           totalPoint = tempPoint;
+           index1 = i;
+        }
+    }
+    printf("%d freq %d", max, index1);
+    return index1;
+}
+
+void pushHeadAI(tessera *table, cardObj* arrayObj, int indexCard, int *pointer)
+{
+    table[*pointer] = arrayObj[indexCard].card;
+    *pointer += 1; // VARIFICA CHE QUANDO PUNTA ALL' ELEMENTO 29 NON SIA POSSIBILE INSERIRE O ESSERCI LATRO (OUT OF ARRAY)
+}
+
+void insertdx (cardObj* arrayObj, int indexCard, tessera* table, int *pointer ) {
+    tessera card = arrayObj[indexCard].card;
+    for (int i = 1; i <= arrayObj[indexCard].freq; i++) {
+        if ((i % 2) == 0) {
+            tessera cardRotated = rotateAI(card);
+            printf("[%d|%d]", cardRotated.num1, cardRotated.num2);
+            pushHeadAI(table, arrayObj, indexCard, pointer);
+        }
+        else {
+            pushHeadAI(table, arrayObj, indexCard, pointer);
+        }
+    }
+}
 
 void modAI(tessera* alltessere, int tableSize) {
 
@@ -83,6 +126,7 @@ void modAI(tessera* alltessere, int tableSize) {
 
     // Creazione tavolo e tessere
     tessera* table = creaTable(tableSize);
+    int indexTableAI = 0;
     tessera* AiCards = giveTessereToPlayer(alltessere, tableSize);
     printTessere(AiCards, tableSize);
     puts("\n");
@@ -100,6 +144,16 @@ void modAI(tessera* alltessere, int tableSize) {
     for (int k = 0; k < sizeObj; k++) {
         printf("[%d|%d] -> %d\n", arrayObj[k].card.num1, arrayObj[k].card.num2, arrayObj[k].freq);
     }
+
+    // 2: partita IA
+    //trova la tessera più frequente
+
+    int indexCard = obtainCard(arrayObj, sizeObj);
+    insertdx (arrayObj, indexCard, table, &indexTableAI); //pushhead
+
+    puts("\n\n");
+
+    printTessere(table, tableSize);
 
     // FREE ALL MALLOC CREATED!!!
     // I WANT TO BREAK FREE!!!
